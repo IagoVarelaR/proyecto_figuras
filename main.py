e106ab9ed291f900795a5533f44ac714b8e4bc7c
Iago Varela Rodríguez <iago.varela@rai.usc.es>
2024-10-03 12:29:12 +0200

    Agregadas funcionalidades para el triángulo

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from lib import cuadrado
-print("Proyecto figuras")
+from lib.triangulo import get_area as get_area_triangulo
 lado = 6
-print(f"El area de un cuadrado de lado {lado} es: {cuadrado.get_area(lado)}")
+area_cuadrado = get_area(lado)
+print(f"El área del cuadrado de lado {lado} es {area_cuadrado}")
+
+# Código para el triángulo
+base_triangulo = 4
+altura_triangulo = 3
+area_triangulo = get_area_triangulo(base_triangulo, altura_triangulo)
+print(f"El área del triángulo de base {base_triangulo} y altura {altura_triangulo} es {area_triangulo}")
