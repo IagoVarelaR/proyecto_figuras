790f8376eed0dd925aa15ed9c04667e55ccf98cd
Iago Varela Rodríguez <iago.varela@rai.usc.es>
2024-10-03 12:26:26 +0200

    Agregadas funcionalidades para el rectángulo

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from lib import cuadrado
-print("Proyecto figuras")
 lado = 6
-print(f"El area de un cuadrado de lado {lado} es: {cuadrado.get_area(lado)}")
+area_cuadrado = get_area(lado)
+print(f"El área del cuadrado de lado {lado} es {area_cuadrado}")
+
+# Nuevo código para el rectángulo
+base = 4
+altura = 3
+area_rectangulo = get_area(base, altura)
+print(f"El área del rectángulo de base {base} y altura {altura} es {area_rectangulo}")
